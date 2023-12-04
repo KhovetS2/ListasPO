c7b7bdf458a12f21653b3dff5ee7aa8976718be1
Isaque Elis da Silva <90930885+KhovetS2@users.noreply.github.com>
2023-12-03 23:09:02 -0300

    arrumando branch da lista 4

--- atvi-pl-typescript/src/negocio/deletar.ts
+++
@@ -1,5 +0,0 @@
-import Listagem from "./listagem";
-
-export default abstract class Deletar {
-    public abstract deletar(): void
-}
--- atvv-pl-typescript/src/models/Pet.ts
+++
@@ -1,8 +0,0 @@
-export default interface Pet{
-    id: number,
-    nome: string,
-    tipo: string,
-    raca: string,
-    genero: string,
-    cliente_id: number
-}
